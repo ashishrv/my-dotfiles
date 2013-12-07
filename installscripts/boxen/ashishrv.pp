1b37735f841d3ee506a2df2619370859229c4edc
Ashish Vidyarthi <ashish.vid@gmail.com>
2013-12-07 00:45:26 -0800

    boxen customization and update script

---
+++ installscripts/boxen/ashishrv.pp
@@ -0,0 +1,15 @@
+class people::ashishrv {
+  include skitch       # sketch for screen capture
+  include wunderlist   # wanderlist application
+  include virtualbox
+  include textmate
+  include alfred
+  include appcleaner
+  #include dropbox
+  #include evernote
+  include firefox
+  include slate
+
+}
+
+
